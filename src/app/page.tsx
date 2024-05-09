6eeee0e85c0fc246387139333461bc9be7362d84
abrahan villa <ajvilla1999@gmail.com>
2024-05-09 13:30:52 -0500

    favicon updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import { inter, titleFont } from "@/config/fonts";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="">
-      <h1>Hola mundo</h1>
-      <h1>Hola mundo</h1>
+      <h1 className={ inter.className } >Hola mundo</h1>
+      <h1 className={ titleFont.className }>Hola mundo</h1>
     </main>
   );
 }
